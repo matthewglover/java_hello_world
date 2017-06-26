f8755b24c1c89ffc6603508b6a0d30071f27d196
Matt Glover <matthewglover@me.com>
2017-06-26 14:30:19 +0100

    Initial commit

---
+++ src/com/example/helloword/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.example.helloword;
+
+/**
+ * Created by matthewglover on 26/06/2017.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
